56d0cc2dc3ee1c2d1f0b15d9c498922ce2b75859
Kungger-git <matthewriel63@gmail.com>
2021-12-07 16:05:18 +0800

    Update scripts

--- upd-local.sh
+++ upd-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # moving packages to core repository
-mv ./x86_64/* ../zone-repo/x86_64/
+mv ./local-x86_64/* ../zone-repo/x86_64/
 
 # update repository, remove .old & push changes
 (cd ../zone-repo/x86_64/; repo-add zone-repo.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update Local Repository"; .tkn; git push -u origin master)
